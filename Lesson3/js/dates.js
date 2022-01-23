c6ebf60d2b96e9fe355b47aa2286771249bd25d2
sebere <sebere0808@gmail.com>
2022-01-23 10:14:46 -0500

    l3

---
+++ Lesson3/js/dates.js
@@ -0,0 +1,9 @@
+const lastmod = document.querySelector('#lastmod');
+lastmod.textContent = `Last Update ${document.lastModified}`;
+
+const currentYear = new Date();
+document.querySelector("#year").innerText = currentYear.getFullYear();
+
+
+
+
