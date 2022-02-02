e66532880bc9262a25f028b33762245922eec9a0
sebere <sebere0808@gmail.com>
2022-02-01 22:18:23 -0500

    favicon

---
+++ bom/js/list.js
@@ -0,0 +1,26 @@
+const list = document.querySelector('ul');
+const input = document.querySelector('input');
+const button = document.querySelector('button');
+
+button.onclick = function(){
+    let myBook = input.value;
+    input.value = '';
+
+    const listBook = document.createElement('li');
+    const listName = document.createElement('span');
+    const listClik = document.createElement('button');
+
+    listBook.appendChild(listName);
+    listName.textContent = myBook;
+    listBook.appendChild(listClik);
+    listClik.textContent = 'Delete';
+    list.appendChild(listBook);
+
+    listClik.onclick= function(e){
+        list.removeChild(listBook);
+    }
+
+    input.focus();
+
+
+}
