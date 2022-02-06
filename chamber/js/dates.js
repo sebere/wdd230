b77b7d8383d2f9fcd0423cc11d250880fa8d428c
sebere <sebere0808@gmail.com>
2022-02-06 00:28:47 -0500

    date

--- chamber/js/dates.js
+++ chamber/js/dates.js
@@ -5,3 +5,18 @@ const currentYear = new Date();
 document.querySelector("#year").innerText = currentYear.getFullYear();
 
 
+const bannerMessage = (day) => {
+    const ban =  document.querySelector("#banner");
+    const bannerWords = document.createElement("h2");
+    bannerWords.innerText = `🤝🏼 Come join us for the chamber meet and greet ${day} at 7:00 p.m.`;
+    ban.appendChild(bannerWords);
+}
+const dayName = new Date();
+switch(dayName.getDay()){
+    case 1:
+    bannerMessage("Wednesday")
+    break;
+    case 2:
+        bannerMessage("Wednesday")
+        break;
+}
