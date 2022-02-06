de4e4c989ea2939661913612f5666226a9153b2b
sebere <sebere0808@gmail.com>
2022-02-05 23:19:36 -0500

    les

--- chamber/js/menu.js
+++ chamber/js/menu.js
@@ -1,4 +1,4 @@
-const hambutton = document.querySelector('.hamburger');
+const hambutton = document.querySelector('#hamburger');
 const mainnav = document.querySelector('.navig')
 
 hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
