3331e3f11c2ff684fd3be98dbe1070b949d60ba8
sebere <sebere0808@gmail.com>
2022-03-11 22:14:28 -0500

    weahter

--- lesson10/weatherapi.js
+++ lesson10/weatherapi.js
@@ -3,23 +3,12 @@ fetch(apiURL)
   .then((response) => response.json())
     .then((jsObject) => {
         console.log(jsObject);
-        document.querySelector('#tempe').textContent = jsObject.main.temp;
+        document.querySelector('#current-temp').textContent = jsObject.main.temp;
 
         const iconsrc= `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`;
         const desc = jsObject.weather[0].description;
-        document.querySelector('#clima').textContent = iconsrc;
+        document.querySelector('#icon-src').textContent = iconsrc;
         document.querySelector('#weathericon').setAttribute('src', iconsrc);
         document.querySelector('#weathericon').setAttribute('alt', desc);
         document.querySelector('figcaption').textContent = desc;
   });
-
-const t = parseInt(document.querySelector("#tempe").textContent);
-const s = parseInt(document.getElementById("speed").textContent);
-
-if(t <= 50 && s > 3) {
-    const windchill = 35.74 + 0.6215 * t - 35.75 * Math.pow(s,0.16) + 0.4275 * t * Math.pow(s,0.16)
-    document.querySelector("#windchill").innerHTML = `${Math.round(windchill)}&#176;`;
-}
-else {
-    document.querySelector("#windchill").innerHTML = "N/A"
-}
